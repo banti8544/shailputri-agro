10f83d893ed7325b6192714eaecaf801443e7c91
banti8544 <shailputriagro@gmail.com>
2026-09-01 18:32:26 +0530

    Update db.js

--- db.js
+++ db.js
@@ -91,7 +91,7 @@ if (!existingConfig) {
   // Update logo if already exists
   db.prepare(`
     UPDATE company_settings 
-    SET logo_url = 'images/logo.png', signatory_url = 'images/logo.jpg' 
+    SET logo_url = 'images/logo.png', signatory_url = 'images/SAFPL.jpg' 
     WHERE id = 1
   `).run();
 }
